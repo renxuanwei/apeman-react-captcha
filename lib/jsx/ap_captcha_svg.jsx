f26081eff6cf292ab2df6249684fd09f140245b3
okunishinishi <okunishitaka.com@gmail.com>
2015-11-30 16:50:01 +0900

    Add svg generator

--- lib/jsx/ap_captcha_svg.jsx
+++ lib/jsx/ap_captcha_svg.jsx
@@ -32,8 +32,8 @@ let ApCaptchaSvg = React.createClass({
             version: '1.1',
             xmlns: 'http://www.w3.org/2000/svg',
             text: null,
-            width: 240,
-            height: 120
+            width: 256,
+            height: 92
         };
     },
 
@@ -42,12 +42,28 @@ let ApCaptchaSvg = React.createClass({
             props = s.props;
 
         let color = '#555';
-        let text = props.text.split('').map((letter, i, letters)=> {
+
+        let width = props.width,
+            height = props.height;
+
+        let backgrounds = [0, 1, 2, 3].map((val, i, arr)=> {
+            let len = arr.length;
+            let margin = width * 0.2;
+            let rate = (i + 0.5) / len;
+            return s._stripeBlock(rate, color, {
+                key: `bg-${i}`,
+                width: width / len + (margin * 2),
+                height: height + (margin * 2),
+                x: width * rate - margin,
+                y: 0 - margin
+            });
+        });
+
+        let texts = props.text.split('').map((letter, i, letters)=> {
             let indices = [0, 1, 2, 3, 4, 5],
                 real = randomval.randomInt(0, indices.length - 1);
-
             let texts = indices.map((j) => {
-                let rate = (i + 0.5) / letters.length,
+                let rate = i / letters.length,
                     key = `letter-${i}-${j}`;
                 if (j === real) {
                     return s._renderLetter(letter, rate, {
@@ -65,17 +81,14 @@ let ApCaptchaSvg = React.createClass({
                 <g key={`letter-group-${i}`}>{texts}</g>
             );
         });
-        let width = props.width,
-            height = props.height;
         return (
             <svg version={props.version}
                  width={width}
                  height={height}
                  viewBox={`0 0 ${width} ${height}`}
             >
-                <g>
-                    {text}
-                </g>
+                <g>{backgrounds}</g>
+                <g>{texts}</g>
             </svg>
         )
     },
@@ -88,23 +101,25 @@ let ApCaptchaSvg = React.createClass({
         let s = this,
             props = s.props;
 
-        let padding = 8;
+        let padding = 16;
 
         let w = props.width - (padding * 2),
             h = props.height;
 
-        let moveRange = numcal.min(h / 10, 20),
+        let moveRange = h / 20,
             move = randomval.randomInt.bind(randomval, moveRange * -1, moveRange);
 
-        let x = padding + w * rate + move(),
-            y = h / 2 + move(),
-            rotate = randomval.randomInt(-45, 45);
+        let fontSize = h * 0.8;
+        let x = padding + w * rate + (fontSize / 4),
+            y = fontSize,
+            rotateRange = 40,
+            rotate = randomval.randomInt(-rotateRange, rotateRange);
 
         return (
             <text x={x}
                   y={y}
-                  fontSize={h/3}
-                  transform={`rotate(${rotate}, ${x}, ${y})`}
+                  fontSize={fontSize}
+                  transform={`translate(${move()}, ${move()}) rotate(${rotate}, ${x}, ${y})`}
                 {...textProps}
             >{letter}</text>
         )
@@ -114,6 +129,42 @@ let ApCaptchaSvg = React.createClass({
         const letters = "1234567890abcdefg";
         let len = letters.length;
         return letters[randomval.randomInt(0, len - 1)];
+    },
+
+    _stripeBlock: function (rate, color, blockProps) {
+        let s = this,
+            props = s.props;
+
+        let rotate = randomval.randomInt(-90, 90);
+
+        let lines = [],
+            lineWidth = 1;
+        let w = blockProps.width,
+            h = blockProps.height;
+
+        let left = w * -0.5,
+            right = w * 1.5;
+
+        for (let x = left; x < right; x += lineWidth * 6) {
+            lines.push(
+                <line x1={x}
+                      y1={0}
+                      x2={x}
+                      y2={h}
+                      lineWidth={lineWidth}
+                      key={`line-${x}`}
+                      stroke={color}
+                />
+            );
+        }
+
+        return (
+            <svg {...blockProps}>
+                <g transform={`scale(1.3) rotate(${rotate}, ${blockProps.width / 2}, ${blockProps.height / 2})`}>
+                    {lines}
+                </g>
+            </svg>
+        )
     }
 
 });
